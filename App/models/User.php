e00f94997bb22358208f3e4f7faaba6faf6fa25f
Cleeein <pagaroganjhonclein@gmail.com>
2025-10-04 22:06:44 +0800

    add user method findByEmail

--- App/models/User.php
+++ App/models/User.php
@@ -1,9 +1,34 @@
 <?php 
-    require_once "Member.php";
+    require_once "../";
+    
 
-    class Admin extends Member {
+    class User extends Database {
+        public $user_id = "";
+        public $first_name = "";
+        public $last_name = "";
+        public $middle_name = "";
+        public $email = "";
+        public $password = "";
+        public $role = "";
+        public $created_at = "";
+
+        protected $db;
+
+        public function findByEmail($email) {
+            $sql = "SELECT * FROM members WHERE email = :email";
+
+            $query = $this->connect()->prepare($sql);
+            $query->bindParam(":email", $email);
+
+            if($query->execute()) {
+                return $query->fetch();
+            } else {
+                return null;
+            }
+
+        }
+
         
-
-    };
+    }
 
 ?>
