69b2e873f7aeb7ac27d957b5d3912b10a08a3995
Cleeein <pagaroganjhonclein@gmail.com>
2025-10-05 19:44:56 +0800

    enchance user login design

--- views/auth/login.php
+++ views/auth/login.php
@@ -9,18 +9,17 @@
     $loginErrors = ["email" => "", "password"=> ""];
     $loginError = "";
     if($_SERVER['REQUEST_METHOD'] == 'GET') {
-        $login["email"] = trim(htmlspecialchars($_GET['email']));
-        $login["password"] = trim(htmlspecialchars($_GET['password']));
+        $login["email"] = isset($_GET['email']) ? trim(htmlspecialchars($_GET['email'])) : "";
+        $login["password"] = isset($_GET['password']) ? trim(htmlspecialchars($_GET['password'])) : "";
 
         if(empty($login['email'])) {
             $loginErrors['email'] = "Email is Required.";
-        }
-        if(!filter_var($login['email'], FILTER_VALIDATE_EMAIL)) {
+        } else if(!filter_var($login['email'], FILTER_VALIDATE_EMAIL)) {
             $loginErrors['email'] = "Email is invalid";
         }
 
         if(empty($login['password'])) {
-            $loginErrors['password'] = "Password is Required";
+            $loginErrors['password'] = "Password is Required.";
         }
 
 
@@ -42,20 +41,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gymazing!</title>
-    <style>
-        p {
-            color: red;
-        }
-    </style>
+    <script src="../../public/assets/js/tailwindcss/tailwindcss.js"></script>
 </head>
 <body>
-    <form method="get">
-        <input type="email" name="email" id="" placeholder="Email" value="<?= $login['email'] ?>">
-        <p><?= $loginErrors['email'] ?></p>
-        <input type="password" name="password" id="" placeholder="password" value="<?= $login['password'] ?>">
-        <p><?= $loginErrors['password'] ?></p>
-        <input type="submit" value="Login">
-        <p><?= $loginError ?></p>
-    </form>
+    <main class="min-h-screen w-full bg-[var(--primary)] flex items-center justify-center font-sans">
+        <div class="p-2 h-110 w-100 bg-[var(--primary)] border border-gray-500/50 shadow-md rounded-md ">
+            <form method="GET" class="p-5">
+                <div class="my-8 flex items-center justify-center">
+                    <h1 class="text-4xl text-blue-600 font-bold ">Gymazing!</h1>
+                </div>
+                <div class="m-2 ">
+                    <label for="email" class="text-zinc-700 text-lg font-900">Email</label>
+                    <input type="email" name="email" placeholder="example@email.com" value="<?= $login['email'] ?? "" ?>" class="p-2 w-full rounded-md border border-gray-500/50 bg-gray-300/50">
+                    <p class="text-red-500 text-sm m-1"><?= $loginErrors['email'] ?></p>
+                </div>
+                <div class="m-2">
+                    <label for="password" class="text-zinc-700 text-lg">Password</label>
+                    <input type="password" name="password" id="password" placeholder="********" value="<?= $login['password'] ?>" class="p-2 w-full rounded-md border border-gray-500/50 bg-gray-300/50">
+                    <p class="text-red-500 text-sm m-1" ><?= $loginErrors['password'] ?></p>
+                </div>
+                <div class="flex items-center justify-center">
+                    <input type="submit" value="Login" class="h-10 w-30 border bg-blue-600 text-white rounded-lg">
+                </div>
+                <!-- <div class="p-2">
+                    <p class="text-red-500 text-sm m-1"><?= $loginError ?></p>
+                </div> -->
+                <div class="flex-1 text-center items-center justify-center">
+                    <a href="#" class="text-sm text-blue-500 underline">Forgot Password?</a>
+                    <p class="text-sm">Create new Account. <a href="register.php" class="text-blue-500 underline">Sign up</a></p>
+                </div>
+            </form>
+        </div>
+    </main>
+    <div class="none">
+
+    </div>
 </body>
 </html>
